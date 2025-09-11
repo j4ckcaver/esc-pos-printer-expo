f984e2bf4c3685d1ed0fb64e6a5a086514ebcc1c
Md Muhaiminul <muhai4959@gmail.com>
2025-09-11 13:12:15 +0600

    54

--- App.tsx
+++ App.tsx
@@ -1,23 +1,14 @@
 import { useEffect } from 'react';
 import { View } from "react-native";
 import { useFonts } from "expo-font";
-import { Provider } from 'react-redux';
-import { persistor, store } from '@/redux/store';
 import * as SplashScreen from 'expo-splash-screen';
 import {  NavigationContainer } from '@react-navigation/native';
-import { configureReanimatedLogger, ReanimatedLogLevel } from 'react-native-reanimated';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { PersistGate } from 'redux-persist/integration/react';
 import RootNavigator from '@/navigators/RootNavigator';
 import { ThemeProvider } from './context/ThemeContext';
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
-
-configureReanimatedLogger({
-  level: ReanimatedLogLevel.warn,
-  strict: false
-});
 
 export default function App() {
 
@@ -39,15 +30,11 @@ export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <ThemeProvider>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
             <NavigationContainer>
               <GestureHandlerRootView style={{ flex: 1 }}>
                 <RootNavigator />
               </GestureHandlerRootView>
             </NavigationContainer>
-          </PersistGate>
-        </Provider>
       </ThemeProvider>
     </View>
   );
